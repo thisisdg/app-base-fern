dc954d7b79bc27981c70a4b65357a3428c26a562
Yash Karanke <dex.papa@gmail.com>
2020-11-20 18:54:18 +0530

    Applied the concept of Barrel maybe

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import logo from './images/logo.svg';
 import './App.css';
-
+import Form from './components/forms/Form';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <div id="hello"></div>
           Learn React
-        
+        <Form/>
       </header>
     </div>
   );
